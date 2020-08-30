8aaf5e8d4c11f03670392ae6d500f949fe53613d
anubhav singh <anubhavsingh230@gmail.com>
2020-08-31 03:10:39 +0530

    ADDED: Test case to comparing feet and inch

--- src/QuantityMeasurement.js
+++ src/QuantityMeasurement.js
@@ -1,19 +1,27 @@
 class QuantityMeasurement{
-   feet(value){
+    feet(value){
+       if(value == null)
+       return false;
        return value*12;
-   };
+    };
    
-   inch(value){
+    inch(value){
+        if(value == null)
+        return false;
        return value;
-   };
+    };
 
-   yard(value){
+    yard(value){
+        if(value == null)
+        return false;
        return value*36
-   };
+    };
 
-   centimeter(value){
-    return value*0.4;
-   }
+    centimeter(value){
+        if(value == null)
+        return false;
+        return value*0.4;
+    }
 }
 
 module.exports = QuantityMeasurement;
--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -1,8 +1,21 @@
 const assert = require('chai').assert;
 const QuantityMeasurement = require('../src/QuantityMeasurement');
 const lenght = new QuantityMeasurement();
+
 describe('Quantity Measurement for lenght', () => {
     it('Given same quantity of feet should return equal', () => {
+        let feet1 = lenght.feet(0.0);
+        let feet2 = lenght.feet(0.0);
+        assert.equal(feet1, feet2);
+    });
+
+    it('Given 0 quantity of feet and inch should return equal', () => {
+        let feet1 = lenght.feet(0.0);
+        let feet2 = lenght.inch(0.0);
+        assert.equal(feet1, feet2);
+    });
+
+    it('Given 1 feet and 12 inch should return equal', () => {
         let feet1 = lenght.feet(1.0);
         let feet2 = lenght.inch(12.0);
         assert.equal(feet1, feet2);
